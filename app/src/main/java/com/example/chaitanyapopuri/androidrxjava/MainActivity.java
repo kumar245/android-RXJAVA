a3dc62ca33295271d69c79081f0d0851843febe1
Chaitanya.Popuri <ck1a0245@gmail.com>
2017-10-02 10:49:21 -0600

    ADDING BM

--- app/src/main/java/com/example/chaitanyapopuri/androidrxjava/MainActivity.java
+++ app/src/main/java/com/example/chaitanyapopuri/androidrxjava/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.chaitanyapopuri.androidrxjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
