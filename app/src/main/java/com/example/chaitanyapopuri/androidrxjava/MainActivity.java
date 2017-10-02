6cd5a7195288faf150bd3f78ff6db1e016c6b988
Chaitanya.Popuri <ck1a0245@gmail.com>
2017-10-02 10:44:02 -0600

    Adding EM

--- app/src/main/java/com/example/chaitanyapopuri/androidrxjava/MainActivity.java
+++ app/src/main/java/com/example/chaitanyapopuri/androidrxjava/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.chaitanyapopuri.androidrxjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
