71ce77c6108fdba0f1b2a44dafb3e6fa9884aaae
Chaitanya.Popuri <ck1a0245@gmail.com>
2017-10-02 15:01:23 -0600

    Modify EM

--- app/src/main/java/com/example/chaitanyapopuri/androidrxjava/MainActivity.java
+++ app/src/main/java/com/example/chaitanyapopuri/androidrxjava/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText = (EditText) findViewById(R.id.et);
+      editText= (EditText) findViewById(R.id.edit);
     }
 }
